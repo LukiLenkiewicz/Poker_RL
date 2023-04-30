6c769aafa1152a855f9b3ffd3d71721398e42f27
Łukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2023-04-30 20:35:17 +0200

    agent added

---
+++ src/agent.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from constants import ACTIONS
+
+class Agent:
+    def __init__(self, name="player"):
+        self.name = name
+        self._cards = []
+        self.cash = 0
+
+    def add_card(self, card):
+        self._cards.append(card)
+    
+    def remove_cards(self):
+        self._cards.clear()
+
+    def get_hand(self):
+        # TODO: code figure getter
+        pass
+
+    def make_action(self):
+        pass
+
+    def _raise(self):
+        pass
+
+    def _check(self):
+        pass
+    
+    def _call(self):
+        pass
+
+    def _fold(self):
+        pass
+
+
+class RandomAgent(Agent):
+    def __init__(self):
+        super().__init__()
+
+    def make_action(self):
+        action = np.random.choice(ACTIONS)
+        
